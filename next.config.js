d95963f1786f0e332590e832c742dbdf3d58bc16
kim-0617 <kkk5993@naver.com>
2023-03-12 15:45:48 +0900

    :tada: create-next-app 여러 패키지 설치, next 추천 eslint 설치, ts 적용, 여러 파일 정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    domains: ['lecture-1.vercel.app', '*'],
+  },
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
