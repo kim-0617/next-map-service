cd615e30495894f25a6ea9fd53551fa88321f048
kim-0617 <kkk5993@naver.com>
2023-03-20 00:02:30 +0900

    app.tsx에 seo 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import '@/scss/style.scss';
+import { NextSeo } from 'next-seo';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextSeo
+        title={'naver 지도 서비스'}
+        description="naver 지도 서비스 메인 페이지"
+        canonical={`https://localhost:3000`}
+        openGraph={{
+          url: `https://localhost:3000`,
+        }}
+      />
+      <Component {...pageProps} />;
+    </>
+  );
 }
