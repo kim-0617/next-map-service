8fd8da9b1dbdee28a63a75bbec07641f13b5ac62
kim-0617 <kkk5993@naver.com>
2023-03-20 00:03:18 +0900

    헤더부분 스타일과 컴포넌트 완성

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,25 +1,38 @@
+import useMap from '@/hooks/useMap';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 interface Props {
+  onClickLogo?: () => void;
   rightElements?: React.ReactElement[];
 }
 
-function Header({ rightElements }: Props) {
+function Header({ rightElements, onClickLogo }: Props) {
+  const { resetMapOptions, getMapOptions } = useMap();
+
+  const router = useRouter();
+
   return (
-    <header>
-      <div className="logo">
-        <Link href={'/'}>
+    <header className="header">
+      <div className="flexItem">
+        <Link
+          href={'/'}
+          onClick={onClickLogo}
+          className="box"
+          aria-label="홈으로 이동"
+        >
           <Image
             src="https://lecture-1.vercel.app/inflearn.png"
             alt="logo"
             width={110}
             height={20}
+            priority
           />
         </Link>
       </div>
-      {rightElements && <div className="icons">{rightElements}</div>}
+      {rightElements && <div className="flexItem">{rightElements}</div>}
     </header>
   );
 }
